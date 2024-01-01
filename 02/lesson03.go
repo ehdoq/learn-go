c2ed4878574ac56d0eea568c065f6ea5353b3523
Mustafa Gedik <mstf.gdik19@gmail.com>
2024-01-01 13:28:08 +0000

    oop 3

---
+++ 02/lesson03.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"learn-golang/entities"
+)
+
+func main() {
+	circle1 := entities.Circle{Name: "Circle 1", R: 4}
+	circle2 := entities.Circle{Name: "Circle 2", R: 7}
+	rectangle1 := entities.Rectangle{Name: "Rectangle 1", A: 4, B: 10}
+	rectangle2 := entities.Rectangle{Name: "Rectangle 2", A: 2, B: 7}
+	square1 := entities.Square{Name: "Square 1", A: 6}
+	square2 := entities.Square{Name: "Square 2", A: 10}
+
+	geometries := []entities.Geometry{circle1, rectangle1, square1, rectangle2, square2, circle2}
+
+	for _, geometry := range geometries {
+		fmt.Println(geometry.GetName())
+		fmt.Printf("Area: %0.2f\n", geometry.Area())
+		fmt.Printf("Perimeter: %0.2f\n", geometry.Perimeter())
+		fmt.Println("----------------------")
+	}
+}
